f3160b9ce395936d596adbdf631f86124a62cf5f
bellhill39 <playyboyy39@gmail.com>
2019-04-30 21:34:24 +0530

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,39 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+    .styles([
+
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+        'libs/styles.css',
+
+
+
+
+
+    ],'./public/css/libs.css') // location where all the files be compile to
+
+
+
+        .scripts([
+
+
+        'libs/jquery.js',
+        'libs/bootstrap.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+
+
+
+
+        ],'./public/js/libs.js')  // location where all the files be compile to
+
+
+
 });
